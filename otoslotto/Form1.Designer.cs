37a4bf52ab32c81693c7946713cc4a40fb6ef917
HATOSS307 <HATOSS307@gmail.com>
2023-02-20 09:52:21 +0100

    Kész

---
+++ otoslotto/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace otoslotto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.talalt = new System.Windows.Forms.Label();
+            this.megjatszott = new System.Windows.Forms.Label();
+            this.nyeroszamok = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // talalt
+            // 
+            this.talalt.AutoSize = true;
+            this.talalt.ForeColor = System.Drawing.Color.Red;
+            this.talalt.Location = new System.Drawing.Point(402, 401);
+            this.talalt.Name = "talalt";
+            this.talalt.Size = new System.Drawing.Size(51, 13);
+            this.talalt.TabIndex = 0;
+            this.talalt.Text = "Találat ->";
+            // 
+            // megjatszott
+            // 
+            this.megjatszott.AutoSize = true;
+            this.megjatszott.Location = new System.Drawing.Point(402, 375);
+            this.megjatszott.Name = "megjatszott";
+            this.megjatszott.Size = new System.Drawing.Size(73, 13);
+            this.megjatszott.TabIndex = 1;
+            this.megjatszott.Text = "Megjátszott ->";
+            // 
+            // nyeroszamok
+            // 
+            this.nyeroszamok.AutoSize = true;
+            this.nyeroszamok.ForeColor = System.Drawing.Color.Green;
+            this.nyeroszamok.Location = new System.Drawing.Point(402, 350);
+            this.nyeroszamok.Name = "nyeroszamok";
+            this.nyeroszamok.Size = new System.Drawing.Size(83, 13);
+            this.nyeroszamok.TabIndex = 2;
+            this.nyeroszamok.Text = "Nyerőszámok ->";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Location = new System.Drawing.Point(210, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Sorsolás";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 322);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nyeroszamok);
+            this.Controls.Add(this.megjatszott);
+            this.Controls.Add(this.talalt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label talalt;
+        private System.Windows.Forms.Label megjatszott;
+        private System.Windows.Forms.Label nyeroszamok;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ otoslotto/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace otoslotto
+{
+    public partial class Form1 : Form
+    {
+        public readonly Random random = new Random();
+        public readonly int[] selectedNumbers = new int[5];
+
+        public int kivalasztott = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            int size = 40;
+            int num = 1;
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int a = 0; a < 16; a++)
+                {
+                    Button button = new Button()
+                    {
+                        Bounds = new Rectangle(a * size, i * size, size, size),
+                        Text = num.ToString(),
+                        Font = new Font("Arial", 15, FontStyle.Bold),
+                        FlatStyle = FlatStyle.Flat,
+                        Cursor = Cursors.Hand,
+                        Tag = num
+                    };
+
+                    button.FlatAppearance.BorderSize = 0;
+
+                    button.Click += (o, e) => {
+                        if (o is Button clicked)
+                        {
+                            if (clicked.BackColor == Color.Red)
+                            {
+                                clicked.BackColor = Color.Transparent;
+
+                                selectedNumbers[kivalasztott - 1] = -1;
+                                kivalasztott--;
+                                return;
+                            }
+
+                            if (clicked.Tag is int number)
+                            {
+                                if (kivalasztott >= 5)
+                                {
+                                    MessageBox.Show("Maximum 5 db számot választhat.", "Sorsolás", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    return;
+                                }
+
+                                clicked.BackColor = Color.Red;
+
+                                selectedNumbers[kivalasztott] = number;
+                                kivalasztott++;
+                            }
+                        }
+                    };
+
+                    panel1.Controls.Add(button);
+                    num++;
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            talalt.Text = "Találat ->";
+            if (kivalasztott < 4 )
+            {
+                MessageBox.Show("Legalább 4 számot kell választania a sorsoláshoz.", "Sorsolás", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            int[] szamok = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                int rd = random.Next(0, 91);
+
+                foreach (int one in szamok)
+                {
+                    while (one == rd)
+                    {
+                        rd = random.Next(0, 91);
+                    }
+                }
+                szamok[i] = rd;
+
+            }
+            var filtered = selectedNumbers.Where(one => one != -1);
+
+            megjatszott.Text = $"Megjátszott -> {string.Join(", ", filtered)}";
+            nyeroszamok.Text = $"Nyerőszámok -> {string.Join(", ", szamok)}";
+
+            string tal = "";
+
+            foreach (int one in filtered)
+            {
+                foreach (int ran in szamok)
+                {
+                    if (one == ran)
+                    {
+                        if (tal.Length != 0)
+                        {
+                            tal += ", ";
+                        }
+
+                        tal += $"{one}";
+                    }
+                }
+            }
+
+            if (tal.Length != 0)
+            {
+                talalt.Text = $"Találat -> {tal}";
+            }
+        }
+    }
+}
